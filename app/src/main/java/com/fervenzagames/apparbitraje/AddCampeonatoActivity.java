5571c32db05d9fef08336c02c9a559fb31b67628
Carlos Cid Samamed <carlos.ps2@gmail.com>
2018-10-18 14:18:47 +0200

    Añadida comprobación para evitar duplicados a la hora de guardar campeonatos en la BD. Si existe un campeonato con el mismo nombre aparece un mensaje en pantalla(Toast) informando de que ya existe un campeonato con ese nombre en la BD.

--- app/src/main/java/com/fervenzagames/apparbitraje/AddCampeonatoActivity.java
+++ app/src/main/java/com/fervenzagames/apparbitraje/AddCampeonatoActivity.java
@@ -1,6 +1,7 @@
 package com.fervenzagames.apparbitraje;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,8 +14,12 @@ import android.widget.Toast;
 
 import com.fervenzagames.apparbitraje.Models.Arbitros;
 import com.fervenzagames.apparbitraje.Models.Campeonatos;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
@@ -79,14 +84,40 @@ public class AddCampeonatoActivity extends AppCompatActivity {
 
         if(!TextUtils.isEmpty(nombre) && !TextUtils.isEmpty(fecha) && !TextUtils.isEmpty(lugar)){
 
-            String id = campsDB.push().getKey();
+            final String id = campsDB.push().getKey();
             List<Arbitros> listaArbitros = null;
 
-            Campeonatos camp = new Campeonatos(id, nombre, fecha, lugar, tipo, listaArbitros);
+            final Campeonatos camp = new Campeonatos(id, nombre, fecha, lugar, tipo, listaArbitros);
 
-            campsDB.child(id).setValue(camp);
+            // Comprobar si existe este campeonato en la BD
+            // Creamos la consulta que nos devuelve el primer campeonato que encuentre con ese nombre.
+            Query consulta = campsDB
+                    .orderByChild("nombre")
+                    .equalTo(nombre)
+                    .limitToFirst(1);
 
-            Toast.makeText(this, "Campeonato añadido a la BD", Toast.LENGTH_LONG).show();
+            consulta.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        Toast.makeText(AddCampeonatoActivity.this,
+                                "Ya existe un Campeonato en la BD con esos datos. Compruebe el formulario...",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        // Si no existe ningún campeonato con ese nombre se almacenan sus datos en la BD
+
+                        campsDB.child(id).setValue(camp);
+
+                        Toast.makeText(AddCampeonatoActivity.this, "Campeonato añadido a la BD", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
 
         } else {
             Toast.makeText(AddCampeonatoActivity.this, "Debe rellenar el formulario en su totalidad antes de pulsar el botón de Guardar", Toast.LENGTH_LONG).show();
@@ -109,5 +140,40 @@ public class AddCampeonatoActivity extends AppCompatActivity {
         } else {
             Toast.makeText(AddCampeonatoActivity.this, "Debe rellenar el formulario en su totalidad antes de pulsar el botón de Añadir Modalidades", Toast.LENGTH_LONG).show();
         }
+    }
+
+    // Para añadir un campeonato a la BD deberé asegurarme de que existe otro campeonato con el mismo nombre en la BD. Así evitaremos duplicados.
+    private boolean comprobarDuplicados(Campeonatos camp)
+    {
+        // Obtenemos el nombre del campeonato que queremos guardar en la BD
+        String nombre = mNombre.getEditText().getText().toString(); // Nombre a buscar en los campeonatos de la BD.
+
+        // Creamos la consulta que nos devuelve el primer campeonato que encuentre con ese nombre.
+        Query consulta = campsDB
+                .orderByChild("nombre")
+                .equalTo(nombre)
+                .limitToFirst(1);
+
+        // Comprobamos si la consulta ha encontrado algún campeonato con ese nombre o no.
+        boolean existe;
+        consulta.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                boolean e;
+                if(dataSnapshot.exists()){ // Existe un campeonato en la BD con ese nombre
+                    e = true;
+                } else {
+                    e = false;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        return false;
     }
 }
