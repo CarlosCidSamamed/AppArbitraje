ae5753af76bfe6b0e2afd87c4bb49ab973aabbbc
Carlos Cid Samamed <carlos.ps2@gmail.com>
2018-11-22 07:48:36 +0100

    Árbitros- Listado OK           Detalle en proceso
    Registro modificado --> Crea Usuario y Árbitro asociado.

--- app/src/main/java/com/fervenzagames/apparbitraje/User_Activities/LoginActivity.java
+++ app/src/main/java/com/fervenzagames/apparbitraje/User_Activities/LoginActivity.java
@@ -13,11 +13,21 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.fervenzagames.apparbitraje.MainActivity;
+import com.fervenzagames.apparbitraje.Models.Arbitros;
 import com.fervenzagames.apparbitraje.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -31,6 +41,10 @@ public class LoginActivity extends AppCompatActivity {
     private ProgressDialog mLoginProgress;
 
     private FirebaseAuth mAuth;
+
+    private FirebaseUser mCurrentUser;
+
+    private DatabaseReference mArbitrosDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +99,12 @@ public class LoginActivity extends AppCompatActivity {
 
                     mLoginProgress.dismiss();
 
+                    // Modificar el campo conectado del Árbitro en la BD. CONECTADO --> TRUE
+                    // Usaremos updateChildren especificando la ruta donde queremos actualizar los datos para no modificar los demás datos.
+
+                    /*String uid = mCurrentUser.getUid();
+                    actualizarEstadoArbitro(uid, true);*/
+
                     Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
                     mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(mainIntent);
@@ -98,5 +118,35 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void actualizarEstadoArbitro(String uid, final boolean estado){
+        // Como parámetros se pasan:
+        // 1. el UID que permite identificar al usuario y al árbitro correspondiente.
+        // 2. el nuevo estado del árbitro.
+        final String id = uid;
+
+        // El proceso de actualizar usando updateChildren lo he cogido de la documentación de Firebase: https://firebase.google.com/docs/database/android/read-and-write?hl=es-419
+
+        mArbitrosDB.addListenerForSingleValueEvent(new ValueEventListener() { //Listens once for a single event.
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Recuperamos los datos del árbitro
+                Arbitros arbi = dataSnapshot.getValue(Arbitros.class);
+                // Modificamos el estado
+                arbi.setConectado(estado);
+                // Actualizamos los datos del Árbitro en la BD con el nuevo estado de conexión.
+                Map<String, Object> nuevoArbi = arbi.toMap();
+                Map<String, Object> updates = new HashMap<>();
+                updates.put("/arbitraje/arbitros/" + id, nuevoArbi);
+                mArbitrosDB.updateChildren(updates);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
