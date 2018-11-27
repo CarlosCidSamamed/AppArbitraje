5febf76586e4238618bfa42814efd0f41b3ab084
Carlos Cid Samamed <carlos.ps2@gmail.com>
2018-11-27 11:59:36 +0100

    Árbitros- Asignar en proceso
    Filtro Nivel y Cargo Funcional
    Carga de árbitro seleccionado en buscador Funcional
    Guardado de Datos en proceso

--- app/src/main/java/com/fervenzagames/apparbitraje/Add_Activities/AsignarArbitroActivity.java
+++ app/src/main/java/com/fervenzagames/apparbitraje/Add_Activities/AsignarArbitroActivity.java
@@ -4,8 +4,13 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -39,6 +44,13 @@ public class AsignarArbitroActivity extends AppCompatActivity {
     private AutoCompleteTextView mBuscador;
     private List<String> mListaDNIs;
 
+    private Button mAplicarFiltroBtn;
+    private Spinner mNivelSpinner;
+    private Spinner mCargoSpinner;
+
+    private String mIdZonaCombate; // Para almacenar el id de la Zona de Combate que se selecciona en la ListView correspondiente.
+    private String mIdArbi;        // Para almacenar el id del Árbitro que se selecciona en la ListView correspondiente.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,12 +72,19 @@ public class AsignarArbitroActivity extends AppCompatActivity {
         mBuscador = findViewById(R.id.asignar_arb_buscador);
         mListaDNIs = new ArrayList<>();
 
+        mAplicarFiltroBtn = findViewById(R.id.asignar_arb_aplicarFiltro_btn);
+        mNivelSpinner = findViewById(R.id.asignar_arb_filtroNivelSpinner);
+        mCargoSpinner = findViewById(R.id.asignar_arb_filtroCargoSpinner);
+
+        mIdZonaCombate = "";
+        mIdArbi = "";
+
         mNombre.setText(getIntent().getStringExtra("NombreCampeonato"));
         // mZonaCombate.setText(getIntent().getStringExtra("numZonasCombate"));
 
         // Generar array de zonas de combate
         int numZonas = Integer.parseInt(getIntent().getStringExtra("numZonasCombate"));
-        String[] arrayZonas = new String[numZonas];
+        final String[] arrayZonas = new String[numZonas];
         String n = String.valueOf(arrayZonas.length);
         // mZonaCombate.setText(n);
         for(int i = 0; i < arrayZonas.length; i++){
@@ -75,15 +94,102 @@ public class AsignarArbitroActivity extends AppCompatActivity {
         }
 
         // Crear el adapter para cargar los datos del array en un ListView
-        ArrayAdapter adapterZonas = new ZonasCombateList(this, arrayZonas);
+        final ArrayAdapter adapterZonas = new ZonasCombateList(this, arrayZonas);
         adapterZonas.setDropDownViewResource(R.layout.zona_combate_single_layout);
         mZonas.setAdapter(adapterZonas);
 
         // Recuperar los DNIs de los Árbitros para el autocomplete del buscador
         recuperarDNIsArbis();
         // Asignar los datos de la lista de DNIs al AutoCompleteTextView mediante un ArrayAdapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListaDNIs);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListaDNIs);
         mBuscador.setAdapter(adapter);
+        mBuscador.setThreshold(1); // El autocomplete funciona al escribir el primer carácter correcto.
+
+        // ¿Qué ocurre al seleccionar un DNI en el buscador con autocomplete?
+        mBuscador.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String dni = mListaDNIs.get(position); // Recuperar el DNI seleccionado
+                Query consulta = mArbitrosDB.orderByChild("dni").equalTo(dni); // Crear la consulta
+                realizarConsultaArbis(consulta); // Realizar la consulta
+            }
+        });
+
+        // Cargar los datos correspondientes a los cargos según el nivel
+        mNivelSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:{
+                        ArrayAdapter<CharSequence> adapterUno = ArrayAdapter.createFromResource(AsignarArbitroActivity.this, R.array.tareasNivelUno, android.R.layout.simple_spinner_item);
+                        adapterUno.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        mCargoSpinner.setAdapter(adapterUno);
+                        break;
+                    }
+                    case 1:{
+                        ArrayAdapter<CharSequence> adapterDos = ArrayAdapter.createFromResource(AsignarArbitroActivity.this, R.array.tareasNivelDos, android.R.layout.simple_spinner_item);
+                        adapterDos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        mCargoSpinner.setAdapter(adapterDos);
+                        break;
+                    }
+                    case 2:{
+                        ArrayAdapter<CharSequence> adapterTres = ArrayAdapter.createFromResource(AsignarArbitroActivity.this, R.array.tareasNivelTres, android.R.layout.simple_spinner_item);
+                        adapterTres.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        mCargoSpinner.setAdapter(adapterTres);
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        // Aplicar el filtro al pulsar el botón
+        mAplicarFiltroBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener los datos del spinner de Nivel
+                String nivel = mNivelSpinner.getSelectedItem().toString();
+
+                // Obtener los datos del spinner de Cargo
+                String cargo = mCargoSpinner.getSelectedItem().toString();
+
+                // aplicarFiltro
+                Query consulta = aplicarFiltro(nivel, cargo, "");
+
+                // realizarConsulta
+                realizarConsultaArbis(consulta);
+            }
+        });
+
+
+        // Almacenar el ID de la Zona de Combate al seleccionarlo en su ListView.
+        mZonas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mIdZonaCombate = ((ZonasCombateList) adapterZonas).getItem(position);
+                String nuevaZona = "Zona de Combate -- "  + mIdZonaCombate;
+                mZonaCombate.setText(nuevaZona);
+            }
+        });
+        // Almacenar el ID del Árbitro al seleccionarlo en su ListView.
+        mListaArbis.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Arbitros arbi = mLista.get(position);
+                mIdArbi = arbi.getDni();
+
+                // Comprobar que se haya seleccionado la zona de Combate antes de seleccionar al Árbitro.
+                if(TextUtils.isEmpty(mIdZonaCombate)){
+                    Toast.makeText(AsignarArbitroActivity.this, "Seleccione una Zona de Combate antes de seleccionar al Árbitro deseado.", Toast.LENGTH_SHORT).show();
+                } else {
+                    guardarDatos(mIdZonaCombate, mIdArbi);
+                }
+            }
+        });
 
     }
 
@@ -91,42 +197,10 @@ public class AsignarArbitroActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        // aplicarFiltro...
-        // Prueba Filtro Nivel 2
-        /*final Query consulta = mArbitrosDB
-                .orderByChild("nivel")
-                .equalTo("Nivel 2");*/
+        Query cons =  aplicarFiltro(mNivelSpinner.getSelectedItem().toString(), mCargoSpinner.getSelectedItem().toString(), "");
 
-        final Query consulta = mArbitrosDB
-                .orderByChild("DNI");
+        realizarConsultaArbis(cons);
 
-        // Cargar la lista de árbitros sin filtrar
-        consulta.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                mLista.clear();
-
-                if(!dataSnapshot.exists()){
-                    Toast.makeText(AsignarArbitroActivity.this, "No existen Árbitros que cumplan esas condiciones. Revise el filtro utilizado.", Toast.LENGTH_SHORT).show();
-                } else {
-                    for(DataSnapshot arbitroSnapshot: dataSnapshot.getChildren()){
-                        Arbitros arbi = arbitroSnapshot.getValue(Arbitros.class);
-                        mLista.add(arbi);
-                    }
-
-
-                    ArbitrosList adapter = new ArbitrosList(AsignarArbitroActivity.this, mLista);
-                    adapter.setDropDownViewResource(R.layout.arbitro_single_layout);
-                    mListaArbis.setAdapter(adapter);
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
     }
 
 
@@ -138,9 +212,19 @@ public class AsignarArbitroActivity extends AppCompatActivity {
     //                      la info de ambos campeoantos para ver si coinciden en fecha y/o lugar. No existe el don de la Ubicuidad(;D).
     // El DNI se usará para localizar a un árbitro en particular. Para usar el autocomplete debo recuperar los dni de todos los árbitros en la BD y pasarle dicho array a un
     // ArrayAdapter
-    public void aplicarFiltro(String nivel, String cargo, String DNI){
+    // El método devuelve la Query con los parámetros correctos.
+    public Query aplicarFiltro(String nivel, String cargo, String dni){
 
+        Query consulta = mArbitrosDB
+                .orderByChild("dni");
 
+        if(TextUtils.isEmpty(dni)){ // No se ha especificado un filtro por DNI pero sí por nivel y cargo
+            String nivel_cargo = nivel + "_" + cargo;
+            Query consulta2 = mArbitrosDB.orderByChild("nivel_cargo").equalTo(nivel_cargo);
+            consulta = consulta2;
+        }
+
+        return consulta;
 
     }
     // El DNI se usará para localizar a un árbitro en particular. Para usar el autocomplete debo recuperar los dni de todos los árbitros en la BD y pasarle dicho array a un
@@ -148,7 +232,7 @@ public class AsignarArbitroActivity extends AppCompatActivity {
     public void recuperarDNIsArbis(){
 
         Query consulta = mArbitrosDB
-                .orderByChild("DNI");
+                .orderByChild("dni");
 
         consulta.addValueEventListener(new ValueEventListener() {
             @Override
@@ -175,5 +259,56 @@ public class AsignarArbitroActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    // Método para realizar la consulta a la BD con los filtros o con el buscador de DNIs
+    public void realizarConsultaArbis(Query query){
+        final Query consulta = query;
+
+        // Cargar la lista de árbitros sin filtrar
+        consulta.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mLista.clear();
+
+                if(!dataSnapshot.exists()){
+                    Toast.makeText(AsignarArbitroActivity.this, "No existen Árbitros que cumplan esas condiciones. Revise el filtro utilizado.", Toast.LENGTH_SHORT).show();
+                    mListaArbis.setAdapter(null); // Limpiar ListView si no se encuentran árbitros que cumplan las condiciones.
+                } else {
+                    for(DataSnapshot arbitroSnapshot: dataSnapshot.getChildren()){
+                        Arbitros arbi = arbitroSnapshot.getValue(Arbitros.class);
+                        mLista.add(arbi);
+                    }
+
+
+                    ArbitrosList adapter = new ArbitrosList(AsignarArbitroActivity.this, mLista);
+                    adapter.setDropDownViewResource(R.layout.arbitro_single_layout);
+                    mListaArbis.setAdapter(adapter);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    // Método para guardar los datos del Árbitro seleccionado, es decir, asignar a ese Árbitro al Campeonato y a la Zona de Combate seleccionados.
+    public void guardarDatos(String idZona, String idArbi){
+        Toast.makeText(this, "Guardando datos para Asignar Árbitro... ID Zona --> " + mIdZonaCombate + ", ID Arbi --> " + mIdArbi, Toast.LENGTH_SHORT).show();
+        // Recuperar datos Árbitro
+
+        // Actualizar :
+        // 1. Lista de zonas de combate
+        // 2. Id de la zona de combate actual
+        // 3. IdCamp
+
+        // Localizar el Campeonato deseado
+
+        // Actualizar Lista Árbitros
+
+        // Mostrar mensaje de Éxito / Error
     }
 }
