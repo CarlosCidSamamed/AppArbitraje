d7c8590425eb278546a316854ae34a4a3f71194e
Carlos Cid Samamed <carlos.ps2@gmail.com>
2019-01-25 13:06:02 +0100

    Fragments de la tablet en proceso. UI completa para los fragments de la Tablet.

---
+++ app/src/main/java/com/fervenzagames/apparbitraje/TabletFragments/TabletCombatesListFragment.java
@@ -0,0 +1,24 @@
+package com.fervenzagames.apparbitraje.TabletFragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fervenzagames.apparbitraje.R;
+
+public class TabletCombatesListFragment extends Fragment {
+
+    public TabletCombatesListFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tablet_combates_list_fragment, container, false);
+    }
+}
--- app/src/main/java/com/fervenzagames/apparbitraje/Utils/SectionsPagerAdapterTablet.java
+++ app/src/main/java/com/fervenzagames/apparbitraje/Utils/SectionsPagerAdapterTablet.java
@@ -5,7 +5,10 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.fervenzagames.apparbitraje.TabletFragments.TabletArbisListFragment;
 import com.fervenzagames.apparbitraje.TabletFragments.TabletCampsListFragment;
+import com.fervenzagames.apparbitraje.TabletFragments.TabletCombatesListFragment;
+import com.fervenzagames.apparbitraje.TabletFragments.TabletCompetidoresListFragment;
 import com.fervenzagames.apparbitraje.TabletFragments.TabletNextCupsFragment;
 import com.fervenzagames.apparbitraje.TabletFragments.TabletStatsFragment;
 
@@ -26,19 +29,22 @@ public class SectionsPagerAdapterTablet extends FragmentPagerAdapter {
                 TabletCampsListFragment campsListFragment = new TabletCampsListFragment();
                 return campsListFragment;
             }
-            /*case 2:{ // Listado Árbitros
-                return null;
+            case 2:{ // Listado Árbitros
+                TabletArbisListFragment arbisListFragment = new TabletArbisListFragment();
+                return arbisListFragment;
             }
             case 3:{ // Listado Competidores
-                return null;
+                TabletCompetidoresListFragment competidoresListFragment = new TabletCompetidoresListFragment();
+                return competidoresListFragment;
             }
             case 4:{ // Listado Combates
-                return null;
+                TabletCombatesListFragment combatesListFragment = new TabletCombatesListFragment();
+                return combatesListFragment;
             }
             case 5:{ // Próximos Eventos
                 TabletNextCupsFragment nextCupsFragment = new TabletNextCupsFragment();
                 return nextCupsFragment;
-            }*/
+            }
 
             default:{
                 return null;
@@ -48,7 +54,7 @@ public class SectionsPagerAdapterTablet extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return 2;
+        return 5;
     }
 
     @Nullable
@@ -60,7 +66,7 @@ public class SectionsPagerAdapterTablet extends FragmentPagerAdapter {
             }
             case 1:{
                  return "CAMPEONATOS";
-            }/*
+            }
             case 2:{ // Listado Árbitros
                 return "ÁRBITROS";
             }
@@ -73,7 +79,6 @@ public class SectionsPagerAdapterTablet extends FragmentPagerAdapter {
             case 5:{ // Próximos Eventos
                 return "PRÓXIMOS EVENTOS";
             }
-            */
             default:{
                 return null;
             }
